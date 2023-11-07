ab7b6ebeb6b1c4c539f64e83de40466804723b2d
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-07 09:39:02 +0200

    Final Version

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,55 +9,55 @@
 
 
 
-import SwiftUI  // import all that comes with the SwiftUI framework
+import SwiftUI
 
-struct ContentView: View {  // View is a protocol by SwiftUI -> buttons etc.
-  
-    @State private var checkAmount = 0.0 // @State automaticly watches for chances
+struct ContentView: View {
+    @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 10
     @FocusState private var amountIsFocused: Bool
-    
-    let tipPercentages = [10, 15, 20, 25, 0]
-    
+    @State private var useZeroTip = false
+
+    func updateUseZeroTip() {
+        if tipPercentage == 0 {
+            useZeroTip = true
+        } else {
+            useZeroTip = false
+        }
+    }
+
     var total: Double {
         let tipSelection = Double(tipPercentage)
-        
         let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
-        
         return grandTotal
     }
-    
+
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentage)
-        
         let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
-        
         return amountPerPerson
     }
-    
+
     var body: some View {
-        NavigationStack {
+        NavigationView {
             Form {
                 Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")) // checkt die lokale Währung, guckt ob ein Identifier dafür vorliegt. Wenn ja, spuckt er den aus (wie hier €), wenn nicht gehen wir auf Nummer sicher und geben USD aus
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .keyboardType(.decimalPad)
                         .focused($amountIsFocused)
-                    
+
                     Picker("Number of People", selection: $numberOfPeople) {
                         ForEach(2..<100) {
                             Text("\($0) people")
                         }
                     }
-                //    .pickerStyle(.navigationLink) another possebility
                 }
-                
+
                 Section("How much do you want to tip?") {
-                    
                     Picker("Tip Percentage", selection: $tipPercentage) {
                         ForEach(0..<100) {
                             Text("\($0) % tip")
@@ -65,17 +65,17 @@ struct ContentView: View {  // View is a protocol by SwiftUI -> buttons etc.
                     }
                     .pickerStyle(.navigationLink)
                 }
-                
+
                 Section("Total Amount") {
                     Text(total, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundStyle(useZeroTip ? .red : .black)
                 }
-                
-                
-                Section("Total per Person"){
+
+                Section("Total per Person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 }
             }
-            .navigationTitle("WeSplit") // you have to attache this to the thing inside the NavigationStack
+            .navigationTitle("WeSplit")
             .toolbar {
                 if amountIsFocused {
                     Button("Done") {
@@ -84,9 +84,14 @@ struct ContentView: View {  // View is a protocol by SwiftUI -> buttons etc.
                 }
             }
         }
+        .onChange(of: tipPercentage, perform: { _ in
+            updateUseZeroTip()
+        })
     }
 }
 
-#Preview { // just for design time -> when it's shiped it get's deleeted
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
